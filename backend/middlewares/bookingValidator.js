c9d06c5a5f70362e062ff862e45766d87c3bc935
Manish Bisht <manibisht345@gmail.com>
2026-03-31 17:59:37 +0530

    use Joi from validation

---
+++ backend/middlewares/bookingValidator.js
@@ -0,0 +1,34 @@
+import Joi from "joi";
+
+
+export const validateBooking = (req, res, next) => {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required().messages({
+      "string.empty": "Name  is required",
+      "string.min": "Name should  be at least 3 characters long",
+    }),
+    
+    phone: Joi.string()
+      .pattern(/^[0-9]{10}$/)
+      .required()
+      .messages({
+        "string.pattern.base": "Phone  number must be exactly 10 digits",
+      }),
+
+    email: Joi.string().email().required(),
+    address: Joi.string().required(),
+    date: Joi.string().required(),
+    time: Joi.string().required(),
+    professional: Joi.string().optional(),
+    status: Joi.string().valid("confirmed", "pending", "cancelled").optional(),
+  });
+
+  const { error } = schema.validate(req.body, { abortEarly: false });
+
+  if (error) {
+    const errorMessage = error.details.map((details) => details.message);
+    return res.status(400).json({ success: false, errors: errorMessage });
+  }
+
+  next();
+};
--- backend/routes/bookingRoutes.js
+++ backend/routes/bookingRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { createBooking, getBookings, getBookingById, deleteBooking } from "../controllers/bookingController.js";
 import { protect } from "../middlewares/authMiddleware.js";
+import {validateBooking} from "../middlewares/bookingValidator.js";
 
 const router = express.Router();
 
-router.post("/", protect, createBooking);    
+router.post("/", protect, validateBooking, createBooking);    
 router.get("/my", protect, getBookings);   
 router.get("/:id", protect, getBookingById);
 router.delete("/:id", protect, deleteBooking);
